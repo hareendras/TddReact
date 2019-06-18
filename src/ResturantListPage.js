03fc8a256ed4ff2a83527b8fe1b10eff9362e7b4
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2019-06-18 14:19:13 +0530

    progress

--- src/ResturantListPage.js
+++ src/ResturantListPage.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import NewResturantForm from "./NewRestuarantForm";
+import RestuarantList from "./ResturantList";
 
 export default class ResturantListPage extends Component {
+  state = { restuarantNames: [] };
+  handleAddReturant = newRetuarantName => {
+    this.setState(state => ({
+      restuarantNames: [newRetuarantName, ...state.restuarantNames]
+    }));
+  };
   render() {
+    const { restuarantNames } = this.state;
     return (
       <div>
         <button data-test="addRestuarantButton">Add Retuarant</button>
-        <NewResturantForm />
+        <NewResturantForm onSave={this.handleAddReturant} />
+        <RestuarantList restuarants={restuarantNames} />
       </div>
     );
   }
